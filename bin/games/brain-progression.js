1503789843fca8b707bdf512ca9d93fd47715218
eldar935 <kusainov935@gmail.com>
2024-05-12 17:03:00 +0700

    add new game "brain-progression"

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+import {
+    greeting,
+    getGameText,
+    getUserName,
+    getRandom
+} from '../../src/index.js';
+
+greeting();
+const userName = getUserName();
+
+console.log('What number is missing in the progression?');
+
+const getProgression = () => {
+    const progression = [];
+    let elementOfProgression = getRandom(0, 100);
+    const step = getRandom(2, 11);
+
+    for (let i = 0; i < getRandom(5, 12); i += 1) {
+        progression.push(elementOfProgression);
+        elementOfProgression += step;
+    }
+
+    const randomIndex = getRandom(0, progression.length);
+    const hiddenNumber = progression[randomIndex];
+    progression[randomIndex] = '..';
+    progression.push(hiddenNumber);
+
+    return progression;
+};
+
+
+const runGameProgression = () => {
+    let countRightAnswers = 0;
+
+    while (countRightAnswers < 3) {
+        const progression = getProgression();
+        const progressionText = `${progression.slice(0, progression.length - 1)}`;
+        const expected = progression[progression.length - 1];
+        const result = getGameText(userName, progressionText, expected);
+        console.log(result);
+
+        if (result === 'Correct!') {
+            countRightAnswers += 1;
+        } else {
+            countRightAnswers = 0;
+        }
+    }
+
+    console.log(`Congratulations, ${userName}!`);
+};
+
+runGameProgression();
